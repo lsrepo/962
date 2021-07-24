66d27faf8864f9478b4fface5c41ec7330c99838
pak <radishlau@gmail.com>
2021-07-24 23:13:52 +0800

    Provide ETAs for 962

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import eta from './services/eta';
+
+function BusEtaInfo({ eta, route, dest, currentTime = new Date() }) {
+  const waitingTimeInMinutes = (new Date(eta) - currentTime) / 1000 / 60
+
+  return <tr className="bus-eta-info--container">
+    <td><strong>{Math.ceil(waitingTimeInMinutes)} min</strong></td>
+    <td>{route}</td>
+    <td>{dest}</td>
+  </tr>
+}
+
+function EtaDashboard({stop}) {
+  const [etaList, setEtaList] = useState([]);
+
+  useEffect(() => {
+    async function fetchEta() {
+      const etaList = await eta.fetchEta(stop)
+      setEtaList(etaList)
+    }
+
+    fetchEta()
+  }, [etaList.length, stop])
+
+  return (
+    <table>
+      <tbody>
+        {etaList
+        .sort((a, b) =>  new Date(a.eta) - new Date(b.eta))
+        .slice(0, 5)
+        .map((eta, i) => <BusEtaInfo
+          eta={eta.eta}
+          route={eta.route}
+          dest={eta.dest_tc}
+          key={i}
+        />)}
+      </tbody>
+    </table>)
+}
+
+EtaDashboard.propTypes = {
+  stop: PropTypes.string
+};
+
+const STOP_GOLD_COAST_OUTBOUND = "001890"
+const STOP_WEST_TUNNEL_INBOUND = "001628"
+const KWONG_SANG_HONG_INBOUND = "002443"
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>→ 港島</h3>
+        <EtaDashboard stop={STOP_GOLD_COAST_OUTBOUND} />
+        <h3>西隧 → 屯門</h3>
+        <EtaDashboard stop={STOP_WEST_TUNNEL_INBOUND}/>
+        <h3>廣生行 → 屯門</h3>
+        <EtaDashboard stop={KWONG_SANG_HONG_INBOUND}/>
       </header>
     </div>
   );
---
+++ src/services/eta.js
@@ -0,0 +1,136 @@
+const mockData = {
+    "001890": [
+        {
+            co: "CTB",
+            route: "962B",
+            dir: "O",
+            seq: 8,
+            stop: "001890",
+            dest_tc: "銅鑼灣(摩頓台)",
+            dest_en: "Causeway Bay (Moreton Terrace)",
+            eta: "2021-07-24T18:00:54+08:00",
+            rmk_tc: "",
+            eta_seq: 1,
+            dest_sc: "铜锣湾(摩顿台)",
+            rmk_en: "",
+            rmk_sc: "",
+            data_timestamp: "2021-07-24T17:58:09+08:00"
+        },
+        {
+            co: "CTB",
+            route: "962B",
+            dir: "O",
+            seq: 8,
+            stop: "001890",
+            dest_tc: "銅鑼灣(摩頓台)",
+            dest_en: "Causeway Bay (Moreton Terrace)",
+            eta: "2021-07-24T18:17:55+08:00",
+            rmk_tc: "",
+            eta_seq: 2,
+            dest_sc: "铜锣湾(摩顿台)",
+            rmk_en: "",
+            rmk_sc: "",
+            data_timestamp: "2021-07-24T17:58:09+08:00"
+        },
+        {
+            co: "CTB",
+            route: "962B",
+            dir: "O",
+            seq: 8,
+            stop: "001890",
+            dest_tc: "銅鑼灣(摩頓台)",
+            dest_en: "Causeway Bay (Moreton Terrace)",
+            eta: "2021-07-24T18:37:55+08:00",
+            rmk_tc: "",
+            eta_seq: 3,
+            dest_sc: "铜锣湾(摩顿台)",
+            rmk_en: "",
+            rmk_sc: "",
+            data_timestamp: "2021-07-24T17:58:09+08:00"
+        }
+    ],
+    "001628": [
+        { "co": "CTB", "route": "962X", "dir": "I", "seq": 13, "stop": "001628", "dest_tc": "屯門(龍門居)", "dest_en": "Tuen Mun (Lung Mun Oasis)", "eta": "2021-07-24T23:15:27+08:00", "rmk_tc": "", "eta_seq": 1, "dest_sc": "屯门(龙门居)", "rmk_en": "", "rmk_sc": "", "data_timestamp": "2021-07-24T23:03:09+08:00" }, { "co": "CTB", "route": "962X", "dir": "I", "seq": 13, "stop": "001628", "dest_tc": "屯門(龍門居)", "dest_en": "Tuen Mun (Lung Mun Oasis)", "eta": "2021-07-24T23:17:37+08:00", "rmk_tc": "", "eta_seq": 2, "dest_sc": "屯门(龙门居)", "rmk_en": "", "rmk_sc": "", "data_timestamp": "2021-07-24T23:03:09+08:00" }, { "co": "CTB", "route": "962X", "dir": "I", "seq": 13, "stop": "001628", "dest_tc": "屯門(龍門居)", "dest_en": "Tuen Mun (Lung Mun Oasis)", "eta": "2021-07-24T23:29:11+08:00", "rmk_tc": "", "eta_seq": 3, "dest_sc": "屯门(龙门居)", "rmk_en": "", "rmk_sc": "", "data_timestamp": "2021-07-24T23:03:09+08:00" },
+        {
+            co: "CTB",
+            route: "962B",
+            dir: "I",
+            seq: 13,
+            stop: "001628",
+            dest_tc: "屯門(置樂花園)",
+            dest_en: "Tuen Mun (Chi Lok Fa Yuen)",
+            eta: "2021-07-24T22:54:56+08:00",
+            rmk_tc: "",
+            eta_seq: 1,
+            dest_sc: "屯门(置乐花园)",
+            rmk_en: "",
+            rmk_sc: "",
+            data_timestamp: "2021-07-24T22:51:09+08:00"
+        },
+        {
+            co: "CTB",
+            route: "962B",
+            dir: "I",
+            seq: 13,
+            stop: "001628",
+            dest_tc: "屯門(置樂花園)",
+            dest_en: "Tuen Mun (Chi Lok Fa Yuen)",
+            eta: "2021-07-24T23:05:02+08:00",
+            rmk_tc: "",
+            eta_seq: 2,
+            dest_sc: "屯门(置乐花园)",
+            rmk_en: "",
+            rmk_sc: "",
+            data_timestamp: "2021-07-24T22:51:09+08:00"
+        },
+        {
+            co: "CTB",
+            route: "962B",
+            dir: "I",
+            seq: 13,
+            stop: "001628",
+            dest_tc: "屯門(置樂花園)",
+            dest_en: "Tuen Mun (Chi Lok Fa Yuen)",
+            eta: "2021-07-24T23:28:49+08:00",
+            rmk_tc: "",
+            eta_seq: 3,
+            dest_sc: "屯门(置乐花园)",
+            rmk_en: "",
+            rmk_sc: "",
+            data_timestamp: "2021-07-24T22:51:09+08:00"
+        }
+    ],
+    "002443": [{ "co": "CTB", "route": "962X", "dir": "I", "seq": 4, "stop": "002443", "dest_tc": "屯門(龍門居)", "dest_en": "Tuen Mun (Lung Mun Oasis)", "eta": "2021-07-24T23:09:42+08:00", "rmk_tc": "", "eta_seq": 1, "dest_sc": "屯门(龙门居)", "rmk_en": "", "rmk_sc": "", "data_timestamp": "2021-07-24T23:05:10+08:00" }, { "co": "CTB", "route": "962X", "dir": "I", "seq": 4, "stop": "002443", "dest_tc": "屯門(龍門居)", "dest_en": "Tuen Mun (Lung Mun Oasis)", "eta": "2021-07-24T23:23:56+08:00", "rmk_tc": "", "eta_seq": 2, "dest_sc": "屯门(龙门居)", "rmk_en": "", "rmk_sc": "", "data_timestamp": "2021-07-24T23:05:10+08:00" }, { "co": "CTB", "route": "962X", "dir": "I", "seq": 4, "stop": "002443", "dest_tc": "屯門(龍門居)", "dest_en": "Tuen Mun (Lung Mun Oasis)", "eta": "2021-07-24T23:44:00+08:00", "rmk_tc": "", "eta_seq": 3, "dest_sc": "屯门(龙门居)", "rmk_en": "", "rmk_sc": "", "data_timestamp": "2021-07-24T23:05:10+08:00" }]
+}
+
+/**
+ * 
+ * @param {*} inbound From Causeway to Tuen Mun
+ * @returns 
+ */
+const fetchEta = async (stop) => {
+    const routes = ["962", "962S", "962X", "962B"]
+
+    const urls = routes.map(route => `https://rt.data.gov.hk/v1/transport/citybus-nwfb/eta/CTB/${stop}/${route}`)
+
+    const promises = urls.map(url => {
+        return fetch(url)
+            .then(response => response.json())
+            .catch(() => [])
+    })
+
+    const etaPromisResults = await Promise.all(promises)
+    const etaList = etaPromisResults.flatMap(response => response.data)
+
+    return etaList
+}
+
+const fetchEtaMock = async (stop) => {
+    return Promise.resolve(mockData[stop])
+}
+
+const exposed = {
+    fetchEta: (process.env.NODE_ENV === 'production' || process.env.REACT_APP_ETA_ENV === 'production' )? fetchEta : fetchEtaMock,
+}
+
+export default exposed
